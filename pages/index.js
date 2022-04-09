bf164c683a28297bc1a077749b697e71cef30c9f
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2022-04-09 18:59:16 +0530

    fix home screen content grid

--- pages/index.js
+++ pages/index.js
@@ -47,6 +47,9 @@ export default function Home({propertiesForSale, propertiesForRent}) {
                 linkName="/search?purpose=for-rent"
                 imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
             />
+            <Flex flexWrap="wrap">
+                {propertiesForSale.map((property) => <Property property={property} key={property.id}></Property>)}
+            </Flex>
         </div>
     )
 }
