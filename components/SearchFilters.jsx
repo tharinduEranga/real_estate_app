64e9b3a5795b13a24182211aabb987435e05f077
Tharindu Eranga <95670397+tharinduwiley@users.noreply.github.com>
2022-04-10 11:25:26 +0530

    add search components without filtering

---
+++ components/SearchFilters.jsx
@@ -0,0 +1,15 @@
+import {useState, useEffect} from "react";
+import {useRouter} from "next/router";
+import {Flex, Box, Text, Icon} from '@chakra-ui/react';
+import {MdCancel} from "react-icons/md";
+import Image from "next/image";
+
+const SearchFilters = () => {
+  return (
+    <div>
+      SearchFilters
+    </div>
+  );
+};
+
+export default SearchFilters;
